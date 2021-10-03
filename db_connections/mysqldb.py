a1737886c7b8a4414aa3139f7a3223ee4db8a144
Brenda <hnin.azali@u.nus.edu>
2021-10-03 14:31:18 +0800

    Cleaned up the comments, Admin now automatically created in database upon initialisation

--- db_connections/mysqldb.py
+++ db_connections/mysqldb.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     # Testing Functions
 
     # # Create customer
-    # db.createCustomer(["brenda3","Brenda3","brenda3@gmail.com","password","1 Street", "4444", "F"])
+    db.createCustomer(["brenda3","Brenda3","brenda3@gmail.com","password","1 Street", "4444", "F"])
     # db.createAdmin(["admin2","Admin2","password", "F", "5555" ])
 
     
